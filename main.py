a5c1dcdd336a7eab08b18c926ab538ab793b2245
dekavc <41837200+dekavc@users.noreply.github.com>
2025-12-29 23:21:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from gtts import gTTS
+import time
 
-# Texto de exemplo
-text = "Olá! Serviço TTS funcionando."
-
-# Gerar áudio
-tts = gTTS(text=text, lang='pt')
-tts.save("audio.ogg")
-
-print("Arquivo audio.ogg gerado com sucesso!")
+# Loop infinito leve para manter o container vivo
+while True:
+    # Texto de teste
+    text = "Olá! Serviço TTS funcionando."
+    
+    # Gerar áudio OGG
+    tts = gTTS(text=text, lang='pt')
+    tts.save("audio.ogg")
+    
+    print("Arquivo audio.ogg gerado com sucesso!")
+    
+    # Espera 60 segundos antes de gerar novamente (evita travar CPU)
+    time.sleep(60)
