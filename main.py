e6ca8eaa40595070b47db132dc6109ba8a5e9ea7
dekavc <41837200+dekavc@users.noreply.github.com>
2025-12-29 23:17:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print("Service alive")
-while True:
-    pass
+from gtts import gTTS
+
+# Texto de exemplo
+text = "Olá! Serviço TTS funcionando."
+
+# Gerar áudio
+tts = gTTS(text=text, lang='pt')
+tts.save("audio.ogg")
+
+print("Arquivo audio.ogg gerado com sucesso!")
